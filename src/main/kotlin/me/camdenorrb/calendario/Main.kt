ec0001637612c5535de3a789ed6626deba02f350
James Bryce <jybryce4@gmail.com>
2020-02-03 22:36:19 -0600

    Finished the freetime command

--- src/main/kotlin/me/camdenorrb/calendario/Main.kt
+++ src/main/kotlin/me/camdenorrb/calendario/Main.kt
@@ -6,6 +6,7 @@ import java.time.DayOfWeek
 object Main {
 
     data class Busy(val userID: Long, val dayOfWeek: DayOfWeek, val timespan: IntRange)
+    data class Free(val userID: Long, val dayOfWeek: DayOfWeek, val timespan: IntRange)
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -49,6 +50,37 @@ object Main {
 
             } else if (it.messageContent.startsWith("-freetime", true)) {
                 // TODO: Return the available freetimes for this week
+                var free = it.messageContent.split(' ').drop(1).windowed(2, 2, false) {
+                    (day, timeFrame) ->
+                    val dayOfWeek = checkNotNull(DayOfWeek.values().find { it.name.equals(day, true) }) {
+                        "Day of week invalid '$day"
+                    }
+
+                    val (start, end) = checkNotNull(timeFrame.split('-').takeIf { it.size == 2 }) {
+                        "Invalid timeframe '$timeFrame"
+                    }
+
+                    var startTime = start.takeWhile { it.isDigit() }.toInt()
+                    var endTime = end.takeWhile { it.isDigit() }.toInt()
+
+                    check(startTime in 1..12) {
+                        "The start time is invalid: '$startTime"
+                    }
+
+                    check(endTime in 1..12) {
+                        "The end time is invalid: '$endTime'"
+                    }
+
+                    if (end.contains("pm", true)) {
+                        if (endTime != 12) {
+                            endTime += 12
+                        }
+                    }
+
+                    Free(0L, dayOfWeek, startTime..endTime)
+                }.sortedBy { it.timespan.first }.groupBy { it.dayOfWeek }
+
+                it.channel.sendMessage("$free")
             }
 
         }
