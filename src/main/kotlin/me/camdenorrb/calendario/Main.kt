55ba954c3fd40e701fdb4190d2a33a5fce936b8e
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2020-02-04 18:46:15 -0600

    Merging schedule stuff

--- src/main/kotlin/me/camdenorrb/calendario/Main.kt
+++ src/main/kotlin/me/camdenorrb/calendario/Main.kt
@@ -29,10 +29,12 @@ object Main {
 
         // busy command:
         client.addMessageCreateListener { event ->
+
             if (event.messageContent.startsWith("-busy", true)) {
                 // -busy Monday 11am-5pm Tuesday 2pm-5pm wednesday n/a thursday 10am-1pm 5pm-9pm
 
                 val schedule = event.messageContent.split(' ').drop(1).windowed(2, 2, false) { (day, timeFrame) ->
+
                     val dayOfWeek = checkNotNull(DayOfWeek.values().find { it.name.equals(day, true) }) {
                         "Day of week invalid '$day"
                     }
@@ -42,7 +44,7 @@ object Main {
                     }
 
                     var startTime = start.takeWhile { it.isDigit() }.toInt()
-                    var endTime = end.takeWhile { it.isDigit() }.toInt()
+                    var endTime   = end.takeWhile { it.isDigit() }.toInt()
 
                     check(startTime in 1..12) {
                         "The start time is invalid: '$startTime"
@@ -52,6 +54,12 @@ object Main {
                         "The end time is invalid: '$endTime'"
                     }
 
+                    if (start.contains("pm", true)) {
+                        if (startTime != 12) {
+                            startTime += 12
+                        }
+                    }
+
                     if (end.contains("pm", true)) {
                         if (endTime != 12) {
                             endTime += 12
@@ -59,21 +67,50 @@ object Main {
                     }
 
                     Busy(0L, dayOfWeek, startTime..endTime)
-                }.sortedBy { it.timespan.first }.groupBy { it.dayOfWeek }
 
-                event.channel.sendMessage("$schedule")
+                }.sortedBy { it.timespan.first }.groupBy { it.dayOfWeek }.toMutableMap()
+
+                //event.channel.sendMessage("$schedule")
 
                 // freetime
+
+
+
+                schedule.forEach { (day, daySchedule) ->
+
+                    val newDaySchedule = mutableListOf<Busy>()
+
+                    daySchedule.indices.forEach indices@{
+
+                        val curr = daySchedule[it]
+                        val next = daySchedule.getOrNull(it + 1)
+
+                        if (next == null) {
+                            newDaySchedule += curr
+                            return@indices
+                        }
+
+                        if (curr.timespan.last == next.timespan.first) {
+                            newDaySchedule
+                        }
+                    }
+                }
 
                 DayOfWeek.values().forEach { dayOfWeek ->
 
                     var lastEndTime = 1
 
-                    schedule[dayOfWeek]?.forEach {
-                        event.channel.sendMessage("Free $dayOfWeek: ${lastEndTime.to12Hour()} to ${it.timespan.first.to12Hour()}")
+                    val message = schedule[dayOfWeek]?.joinToString(prefix = "${dayOfWeek.name.toLowerCase().capitalize()}: ") {
+
+                        val timeRange = lastEndTime..it.timespan.first
                         lastEndTime = it.timespan.last
+
+                        "[${timeRange.first.to12Hour()}, ${timeRange.last.to12Hour()}]"
                     }
 
+
+
+                    event.channel.sendMessage(message)
                 }
 
             } // else if (it.messageContent.startsWith("-freetime", true)) {
